3c39e4fadfb3437d4c9ae549546387668b4bc8ae
gaurab020 <gaurab020@gmail.com>
2022-09-07 13:37:37 +0530

    new changes

--- udemy/1.js
+++ udemy/1.js
@@ -2,6 +2,7 @@ function binarySearch(arr, elem) {
     var start = 0;
     var end = arr.length - 1;
     var middle = Math.floor((start + end) / 2);
+    
     while(arr[middle] !== elem && start <= end) {
         if(elem < arr[middle])
          end = middle - 1;
